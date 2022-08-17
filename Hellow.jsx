8adc7812838622c06e6dc235164b7bf62bc03fd4
usama0011 <69559801+usama0011@users.noreply.github.com>
2022-08-17 11:02:54 +0500

    changing color

--- Hellow.jsx
+++ Hellow.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hellow = () => {
     return (
         <>
-        <div style="color:orange;">Hellow World this is the Fires Introduction of github and first command is git Clone</div>
+        <div style="color:red;">Hellow World this is the Fires Introduction of github and first command is git Clone</div>
         <div> Add anohter div just say Hellow</div>
          <span>Hellow I add a little span above the  code</span>
         </>
