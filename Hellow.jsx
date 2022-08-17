1c88487e131efa68f846dc01ceeb020584e725fe
Usman <za5232208@gmail.com>
2022-08-17 11:28:44 +0500

    Fix merge conflict

--- Hellow.jsx
+++ Hellow.jsx
@@ -5,7 +5,7 @@ const Hellow = () => {
         <>
             <div>Hellow World this is the Fires Introduction of github and first command is git Clone chang same line of codes</div>
             <div> Add anohter div just say Hellow</div>
-            <span>Hellow I add a little span above the  code let now test</span>
+            <span>Lets Figure out what the commit is Changing for us</span>
         </>
     )
 }
