15f5318fb4f0bd92acac967d0b7f550ee2fdaabf
Usman <za5232208@gmail.com>
2022-08-17 11:18:56 +0500

    Changeing or merging conflict files

--- Hellow.jsx
+++ Hellow.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Hellow = () => {
     return (
         <>
-        <div style="color:orange;">Hellow World this is the Fires Introduction of github and first command is git Clone</div>
-        <div> Add anohter div just say Hellow</div>
-         <span>Hellow I add a little span above the  code</span>
+            <div>Hellow World this is the Fires Introduction of github and first command is git Clone chang same line of codes</div>
+            <div> Add anohter div just say Hellow</div>
+            <span>Hellow I add a little span above the  code let now test</span>
         </>
     )
 }
